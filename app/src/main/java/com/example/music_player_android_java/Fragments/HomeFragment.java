242ed1df82232a1df296f5f5d8ffd3b7851f4bf2
Muhammad Prayogo Pangestu <prayogoxyz@gmail.com>
2026-04-20 07:35:24 +0700

    Add RecyclerView and CardView song list to home fragment

--- app/src/main/java/com/example/music_player_android_java/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/music_player_android_java/Fragments/HomeFragment.java
@@ -6,19 +6,42 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.music_player_android_java.R;
+import com.example.music_player_android_java.adapter.SongAdapter;
+import com.example.music_player_android_java.model.Song;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    public HomeFragment() {
-        // Required empty constructor
-    }
+    RecyclerView recyclerView;
+    List<Song> songList;
+
+    public HomeFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        recyclerView = view.findViewById(R.id.recyclerSongs);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        songList = new ArrayList<>();
+
+        songList.add(new Song("Night Drive", "Aryo"));
+        songList.add(new Song("After Rain", "Unknown"));
+        songList.add(new Song("Midnight City", "Dreamer"));
+        songList.add(new Song("Ocean Lights", "Skyline"));
+
+        SongAdapter adapter = new SongAdapter(songList);
+        recyclerView.setAdapter(adapter);
+
+        return view;
     }
 }
---
+++ app/src/main/java/com/example/music_player_android_java/adapter/SongAdapter.java
@@ -0,0 +1,59 @@
+package com.example.music_player_android_java.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.music_player_android_java.R;
+import com.example.music_player_android_java.model.Song;
+
+import java.util.List;
+
+public class SongAdapter extends RecyclerView.Adapter<SongAdapter.ViewHolder> {
+
+    List<Song> songList;
+
+    public SongAdapter(List<Song> songList) {
+        this.songList = songList;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvTitle, tvArtist;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvArtist = itemView.findViewById(R.id.tvArtist);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_song, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        Song song = songList.get(position);
+
+        holder.tvTitle.setText(song.getTitle());
+        holder.tvArtist.setText(song.getArtist());
+    }
+
+    @Override
+    public int getItemCount() {
+        return songList.size();
+    }
+}
