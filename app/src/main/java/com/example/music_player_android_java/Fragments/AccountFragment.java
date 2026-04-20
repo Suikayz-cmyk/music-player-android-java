b516ab4648feb92dc73e8159133cf322b1c6c263
Muhammad Prayogo Pangestu <prayogoxyz@gmail.com>
2026-04-20 15:02:46 +0700

    feat: replace about page with account profile screen

--- app/src/main/java/com/example/music_player_android_java/Fragments/AccountFragment.java
+++ app/src/main/java/com/example/music_player_android_java/Fragments/AccountFragment.java
@@ -1,4 +1,4 @@
-package com.example.music_player_android_java.Fragments;
+package com.example.music_player_android_java.fragments;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,14 +9,14 @@ import androidx.fragment.app.Fragment;
 
 import com.example.music_player_android_java.R;
 
-public class AboutFragment extends Fragment {
+public class AccountFragment extends Fragment {
 
-    public AboutFragment() {}
+    public AccountFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        return inflater.inflate(R.layout.fragment_account, container, false);
     }
 }
--- app/src/main/java/com/example/music_player_android_java/MainActivity.java
+++ app/src/main/java/com/example/music_player_android_java/MainActivity.java
@@ -2,14 +2,10 @@ package com.example.music_player_android_java;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
-import com.example.music_player_android_java.Fragments.AboutFragment;
+import com.example.music_player_android_java.fragments.AccountFragment;
 import com.example.music_player_android_java.Fragments.FavoriteFragment;
 import com.example.music_player_android_java.Fragments.HomeFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -34,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
             } else if (item.getItemId() == R.id.nav_favorite) {
                 loadFragment(new FavoriteFragment());
             } else {
-                loadFragment(new AboutFragment());
+                loadFragment(new AccountFragment());
             }
 
             return true;
