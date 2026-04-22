5aabfa32eb32867a90117354811a543b4d455bfe
Muhammad Prayogo Pangestu <prayogoxyz@gmail.com>
2026-04-22 19:22:20 +0700

    This reverts commit f5455314c03547d6cfa197c7c88c04b42d79059a.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,15 +4,34 @@ plugins {
 
 android {
     namespace = "com.example.music_player_android_java"
-    compileSdk = 36
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
 
     defaultConfig {
         applicationId = "com.example.music_player_android_java"
-        minSdk = 26
+        minSdk = 36
         targetSdk = 36
-
         versionCode = 1
         versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 }
 
