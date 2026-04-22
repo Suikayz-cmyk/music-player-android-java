f5455314c03547d6cfa197c7c88c04b42d79059a
Muhammad Prayogo Pangestu <prayogoxyz@gmail.com>
2026-04-22 19:02:57 +0700

    chore: finalize branding, documentation, and sdk compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,34 +4,15 @@ plugins {
 
 android {
     namespace = "com.example.music_player_android_java"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.music_player_android_java"
-        minSdk = 36
+        minSdk = 26
         targetSdk = 36
+
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
     }
 }
 
