081808af2261d13e4162196c88d73b53a9e1e386
Muhammad Prayogo Pangestu <prayogoxyz@gmail.com>
2026-04-22 16:54:24 +0700

    feat: redesign account page with profile and favorite shortcut

--- app/src/main/java/com/example/music_player_android_java/Fragments/AccountFragment.java
+++ app/src/main/java/com/example/music_player_android_java/Fragments/AccountFragment.java
@@ -8,15 +8,36 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 import com.example.music_player_android_java.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class AccountFragment extends Fragment {
 
     public AccountFragment() {}
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        View view = inflater.inflate(
+                R.layout.fragment_account,
+                container,
+                false
+        );
+
+        View btnFavoriteBox =
+                view.findViewById(R.id.btnFavoriteBox);
+
+        btnFavoriteBox.setOnClickListener(v -> {
+
+            BottomNavigationView nav =
+                    getActivity().findViewById(
+                            R.id.bottom_navigation
+                    );
+
+            nav.setSelectedItemId(R.id.nav_favorite);
+        });
+
+        return view;
     }
 }
